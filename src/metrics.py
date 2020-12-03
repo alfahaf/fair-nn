57b1b20c709694a807c01b62ca58bca72fb8e687
Martin Aumueller <maau@itu.dk>
2020-12-03 22:39:32 +0100

    started work on approximate neighborhood

--- src/metrics.py
+++ src/metrics.py
@@ -11,7 +11,7 @@ def result_prob(results):
                 cnts.setdefault(r, 0)
                 cnts[r] += 1
         n = sum(cnts.values())
-        p = [cnts[r]/n for r in cnts]
+        p = [[r, cnts[r]/n] for r in cnts]
         probs[q] = p
     return probs
 
@@ -20,6 +20,6 @@ def total_variation_dist(g, res):
     all = numpy.zeros(len(g))
     for i in range(len(g)):
         n = int(1/g[i])
-        r = numpy.array(res[i] + [0] * (n - len(res[i])))
+        r = numpy.array([p for _, p in res[i]] + [0] * (n - len(res[i])))
         all[i] = 0.5 * numpy.sum(numpy.abs(r - g[i]))
     return numpy.mean(all)
