e70085de61627ae6e0a247702d2fe059fbf0102a
Martin Aumueller <maau@itu.dk>
2020-12-04 14:20:04 +0100

    Refactored to make callable

--- src/main.py
+++ src/main.py
@@ -5,6 +5,42 @@ from datasets import get_dataset, DATASETS
 from helpers import get_result_fn
 from lsh import LSHBuilder
 
+def run_single_exp(dataset, distance_threshold, lsh_method, k, L, w, validate, report_output):
+    data, queries, _, _ = get_dataset(dataset)
+
+    lsh = LSHBuilder.build(len(data[0]), distance_threshold,
+        k, L, {"type": lsh_method, "w": w}, validate)
+
+    print("Building index...")
+    lsh.preprocess(data)
+
+    candidates = lsh.get_query_size(queries)
+
+    if report_output:
+        print(candidates)
+        exit(0)
+
+    for method in LSHBuilder.methods:
+        print(f"Running (k={args.k}, L={args.L}) with {method}")
+        start = time.time()
+        res = LSHBuilder.invoke(lsh, method, queries, args.runs)
+        print(f"Run finished in {time.time() - start}s.")
+
+        res_fn = get_result_fn(dataset,
+            lsh_method, method, repr(lsh))
+
+        res_dict = {
+            "name": str(lsh),
+            "method" : method,
+            "res": res,
+            "dataset": dataset,
+            "dist_threshold": distance_threshold,
+            "candidates": candidates,
+        }
+
+        with open(res_fn, 'wb') as f:
+            pickle.dump(res_dict, f, pickle.HIGHEST_PROTOCOL)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -55,42 +91,5 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    data, queries, ground_truth, attrs = get_dataset(args.dataset)
-
-    lsh = LSHBuilder.build(len(data[0]), args.distance_threshold,
-        args.k, args.L, {"type": args.method, "w": args.w}, args.validate)
-
-    print("Building index...")
-    lsh.preprocess(data)
-
-    candidates = lsh.get_query_size(queries)
-
-    if args.report_output:
-        print(candidates)
-        exit(0)
-
-    for method in LSHBuilder.methods:
-        print(f"Running (k={args.k}, L={args.L}) with {method}")
-        start = time.time()
-        res = LSHBuilder.invoke(lsh, method, queries, args.runs)
-        print(f"Run finished in {time.time() - start}s.")
-
-        res_fn = get_result_fn(args.dataset,
-            args.method, method, repr(lsh))
-
-        res_dict = {
-            "name": str(lsh),
-            "method" : method,
-            "res": res,
-            "dataset": args.dataset,
-            "dist_threshold": args.distance_threshold,
-            "candidates": candidates,
-        }
-
-        with open(res_fn, 'wb') as f:
-            pickle.dump(res_dict, f, pickle.HIGHEST_PROTOCOL)
-
-
-
-
-
+    run_single_exp(args.dataset, args.distance_threshold, args.method, 
+        args.k, args.L, args.w, args.validate, args.report_output)
