706014050e5c34bcdac258e04b38f4b154483f2c
Martin Aumueller <maau@itu.dk>
2020-12-17 22:17:51 +0100

    Added script to convert pickle to txt for c++

---
+++ src/pickle_to_txt.py
@@ -0,0 +1,17 @@
+# Convert the pickled datasets into txt files used by the C++ code
+import numpy as np
+import pickle
+import sys
+
+
+fn = sys.argv[1]
+with open(fn, 'rb') as f:
+    data, queries, _, _ = pickle.load(f)
+
+gn = fn.split(".")[0]
+
+np.savetxt(f"{gn}-data.txt", data)
+np.savetxt(f"{gn}-queries.txt", queries)
+
+
+
