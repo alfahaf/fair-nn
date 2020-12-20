4d220cc467d61d449c3651956c12be9931dac61a
Martin Aumueller <maau@itu.dk>
2020-12-20 15:49:08 +0100

    Added progress, flag added

--- src/approx_exp.py
+++ src/approx_exp.py
@@ -12,16 +12,17 @@ def run(fn):
         989: 0,
     }
 
-    for _ in range(RUNS):
+    for i in range(RUNS):
+        if i % 100 == 0:
+            print(f"{i}/{RUNS}")
         lsh = LSHBuilder.build(len(data[0]), 0.5, 5, 3, {"type" : "onebitminhash"}, False)
         lsh.preprocess(data)
-        
         res = lsh.opt(queries, 1, False)[0]
         if len(res) > 0:
             res = res[0]
             if res in counter:
                 counter[res] += 1
-    with open(fn) as f:
+    with open(fn, "w") as f:
         f.write("point,count,runs\n")
         for point, count in counter.items():
             f.write(f"{point},{count},{RUNS}\n") 
